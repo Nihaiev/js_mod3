9af52a40a5b22dbdd5de0d23957539c2293c2803
Иван Нигаев <Nihaiev@it.group>
2022-07-04 21:33:59 +0300

    added Example 4

---
+++ js/lesson5.js
@@ -0,0 +1,66 @@
+// const user = { name: 'Mango', age: 20, hoby: 'html', premium: true, };
+
+// // user['mood'] = 'happy'
+// // user['2asdsap'] = 's';
+// user.mod = 'happy';
+// // const key = 'mood';
+// // user[key] = 'happy';
+// user.hobby = 'skydiving';
+// user.premium = false;
+// const keys = Object.keys(user)
+// console.log(keys);
+// for (let key of keys) {
+//     // console.log('key', key);
+//     console.log(`${key}:${key}`);
+// }
+// console.log(user);
+
+// examples 2
+
+// const salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Petes: 130
+// };
+
+// const values = Object.values(salaries)
+// let sum = 0
+
+// for (let val of values) {
+//     console.log('val', val);
+//     sum+=val
+    
+// }
+
+// console.log(sum);
+
+// example 3
+
+// const stones = [
+//     { name: 'Изумруд', price: 1300, quantity: 4 },
+//     { name: 'Брилиант', price: 2700, quantity: 3 },
+//     { name: 'Сапфир', price: 400, quantity: 7 },
+//     { name: 'Щебень', price: 200, quantity: 2 },
+// ];
+
+// function calcTotalPrice(stones, stoneName) {
+//     let sum = 0
+//     for (let stone of stones) {
+        
+//         if (stone.name === stones);
+//         // console.log('stone', stone);
+//         sum = stone.price * stone.quantity
+//         break
+//     }
+//     return sum;
+// }
+// // calcTotalPrice(stones, 'Щебень');
+// console.log(calcTotalPrice(stones, 'Брилиант'));
+
+
+// Example 4
+
+function compare(firstNumber, secondNumber, operation, result) { }
+
+compare("1", "2", "+", "3");
+
---
+++ js/script2.js
@@ -0,0 +1,14 @@
+// const bookShelf = {
+//     books: ["The Last Kingdom"],
+//     getBooks() {
+//         return this.books;
+//     }
+//     addBook(bookName) {
+//         this.books.push(bookName);
+//     },
+//     removeBook(bookName) {
+//         const bookIndex = this.books.indexOf(bookName);
+//         this.books.splice(bookIndex, 1);
+//     },
+// };
+
