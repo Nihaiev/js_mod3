1c72ce3007718113db6703fedfe3534c496cde5e
Иван Нигаев <Nihaiev@it.group>
2022-07-05 23:39:16 +0300

    Update object.js

--- js/object.js
+++ js/object.js
@@ -1,3 +1,9 @@
+/*
+*  Обєкт всьому голова, самий важливий  тип даниї в JS
+*
+* Основи Обєктів
+*/
+
 // const playlist = {
 //     name: 'мій плейлист',
 //     rating: 5,
@@ -95,13 +101,93 @@
 // console.log([] === []); //чому тут false, силочний тип, два місця в памяті різних, порівнюється по посилання - не рівні адреса і до побачення
 // те саме з обєктами, 2 обєкти можуть бути рівні тільки тоді один одному коли це посилання на один і тот же обєкт
 
-const a = { x: 1, y: 2 };
-const b = a;
+// const a = { x: 1, y: 2 };
+// const b = a;
 
-console.log({ a: 1 } === { a: 1 }); // тут ми порівнюєм один літерал a: 1 з іншим a: 1 і вони не рівно false тому що складний тип, обєкти були створені у разних ячейках памяті 
-console.log(b === a);
+// console.log({ a: 1 } === { a: 1 }); // тут ми порівнюєм один літерал a: 1 з іншим a: 1 і вони не рівно false тому що складний тип, обєкти були створені у разних ячейках памяті
+// console.log(b === a);
 
-a.c = 100; 
+// a.c = 100; //у змінну а добав властивість С і присовй значення 100
 
-console.log(a);
-console.log(b);
+// console.log(a);
+// console.log(b);
+
+/*
+* чисто для співбесіди
+*
+*/
+
+// const a = [1, 2, 3];
+// a.hello = ':)';
+
+// console.log(a);
+
+// те ж саме з функцією
+
+// const fn = function () {
+//     console.log('hello');
+// };
+
+// fn.hello = ';)';
+
+// console.dir(fn.hello);
+
+
+/*
+* А тепер поговоримо про методи 
+* Метод - це сама звичайна функція, но працює із властивостями якогось обєкту 
+*/
+
+// const playlist = {
+//     name: 'my playlist',
+//     rating: 5,
+//     tracks: ['track1', 'track2', 'track3'],
+//     trackCount: 3,
+//     // getName: function () { // це олдскульне обявлення метода
+//     //     console.log('Ага, це ж getName :)');
+//     // }
+//     getName: () { // це сучасний виклик методів (метод обєктів) ми будемо використовувати тільки його 
+//      console.log('Ага, це ж getName :)' a);
+//     }
+// };
+
+// // console.log(playlist);
+
+// playlist.getName(5); // викликаємо функцію
+
+
+const playlist = {
+    name: 'my playlist',
+    rating: 5,
+    tracks: ['track1', 'track2', 'track3'],
+    trackCount: 3,
+    changeName(newName) {
+        //  в середині методів обєкту ніколи не використовувати імя самого обєкта, використовуємо this
+        console.log('this в середині changeName:', this);
+        this.name = newName;
+    },
+
+    //  новий метод, добавляэмо трек 
+    // якщо в середині обєкту потрбіно отримати доступ до властивості цього ж обєкту, обєкт який викликав цю функцію ми використовуємо this. і просто імя властивості
+    addTrack(track) {
+        this.tracks.push(track); //  ми спочатку запушили 
+        // this.trackCount = this.tracks.length; //.. потім візьми цю властивість trackCount і кинь туди значення довжини цього масиву но це дуже погане рішення
+    },
+// ще один метод updateRating(){}
+    updateRating(newRating) {
+        this.rating = newRating; // отримуємо доступ до rating
+    },
+// 
+    getTrackCount() {
+        return this.tracks.length;
+    }
+};
+console.log(playlist.getTrackCount());
+playlist.changeName('new name')
+
+
+playlist.addTrack('new track'); // тут ми добавляємо новий трек 
+console.log(playlist.getTrackCount()); 
+
+playlist.updateRating(4);
+console.log(playlist);
