056530bfb6c2268477bcffcad17a3d0ef33ffc98
Иван Нигаев <Nihaiev@it.group>
2022-07-10 19:53:35 +0300

    Update destructuring.js

--- js/destructuring.js
+++ js/destructuring.js
@@ -42,19 +42,19 @@
  * Глибока деструктуризація
  */
 
-const profile = {
-    name: 'Jacques Gluke',
-    tag: 'jgluke',
-    location: 'Ocho Rios, Jamaica',
-    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/r_oy/128.jpg',
-    stats: {
-        followers: 5603,
-        views: 4827,
-        likes: 1308,
-    },
-};
+// const profile = {
+//     name: 'Jacques Gluke',
+//     tag: 'jgluke',
+//     location: 'Ocho Rios, Jamaica',
+//     avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/r_oy/128.jpg',
+//     stats: {
+//         followers: 5603,
+//         views: 4827,
+//         likes: 1308,
+//     },
+// };
 
-const { name, tag, location, avatar, stats:{ followers, views, likes } } = profile; // stats:{ followers, views, likes } глибока деструкрутризація в глибигу
+// const { name, tag, location, avatar, stats:{ followers, views, likes } } = profile; // stats:{ followers, views, likes } глибока деструкрутризація в глибигу
 // у змінній stats лижить обєкт і ми нище деструктуруємо його
 
 // но що якщо нам треба деструкрувати вкладений обєкт stats
@@ -80,17 +80,49 @@ const authors = {
     mango: 6,
 };
 const entries = Object.entries(authors);
-// console.log(entries); //Object.entries повертає масив масивів, де кожен елемент ще один масив нульовим значенням стоїть ключ а першим значення 
-// (const [name, rating] of entries) рівень 3 
-for (const [name, rating] of entries) {
-    // console.log(entry);
+// console.log(entries); //Object.entries повертає масив масивів, де кожен елемент ще один масив нульовим значенням стоїть ключ а першим значення
+// (const [name, rating] of entries) рівень 3
+// for (const [name, rating] of entries) {
+//     // console.log(entry);
 
-    // рівень 2
-    // const [name, rating] = entry;
+//     // рівень 2
+//     // const [name, rating] = entry;
 
-    // рівень 1
-    // const name = entry[0];
-    // const rating = entry[1]
+//     // рівень 1
+//     // const name = entry[0];
+//     // const rating = entry[1]
 
-    console.log(name, rating);
-}
+//     console.log(name, rating);
+// }
+
+
+
+/**
+ * Операція rest (збір)
+ */
+
+const profile = {
+  name: 'Jacques Gluke',
+  tag: 'jgluke',
+  location: 'Ocho Rios, Jamaica',
+  avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/r_oy/128.jpg',
+  stats: {
+    followers: 5603,
+    views: 4827,
+    likes: 1308,
+  },
+};
+
+const { name, tag, location, ...restProps } = profile;
+/**
+ * бувають такі випадки коли треба викинути з обєкта визначені властивості  в локільній змінні
+ * а всі інші властивості зібрати в якийсь інший обєкт 
+ *  */
+// const restProps = {
+//     avatar: profile.avatar,
+//     stats: profile.stats
+// } // ось як воно працює
+
+// console.log(name, tag, location);
+// console.log(restProps);
+// console.log(profile);
