584f2c17e49dce4b2b17df6b1ea5b376291f7a5b
Иван Нигаев <Nihaiev@it.group>
2022-08-14 20:15:45 +0300

    object

---
+++ js/object2.js
@@ -0,0 +1,10 @@
+const playlist = {
+    name: 'Мій плейлист',
+    rating: 5,
+    tracks: ['track-1', 'track-2', 'track-3'],
+    trackCount: 3,
+}
+console.log(playlist.tracks); // достукуємося до значення властивості playlist.tracks значення властивості 
+console.log(playlist.name);
+console.log(playlist.trackCount);
+
