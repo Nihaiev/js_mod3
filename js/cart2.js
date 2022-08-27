48f6d9619ce08bb6f903ee04831c7ad8daaa0a9b
Иван Нигаев <Nihaiev@it.group>
2022-08-27 16:47:38 +0300

    Update cart2.js

--- js/cart2.js
+++ js/cart2.js
@@ -21,7 +21,19 @@ const cart = {
         return this.items
      },
     add(product) { 
-        this.items.push(product)
+        for (const item of this.items){
+           
+            if (item.name === product.name) {
+                item.quantity += 1;
+                return
+            }
+            
+        }
+        const newProduct = {
+            ...product,
+            quantity: 1,
+        }
+        this.items.push(newProduct)
     },
     remove(productName) { 
         const {items} = this
@@ -39,21 +51,50 @@ const cart = {
     clear() {
         this.items=[]
      },
-    countTotalPrice() { },
+    countTotalPrice() { 
+        const { items } = this;
+        let total = 0;
+        for (const {price, quantity} of items) {
+            total +=price*quantity
+            
+        }
+        return total
+
+    },
     increaseQuantity(productName) { },
     decreaseQuantity(productName) {},
 }
 console.log(cart.getItems());
 
+
+cart.add({ name: '🍇', price: 70 })
+cart.add({ name: '🍇', price: 70 })
+cart.add({ name: '🍇', price: 70 })
+cart.add({ name: '🍇', price: 70 })
 cart.add({ name: '🍇', price: 70 })
 cart.add({ name: '🍋', price: 60 })
+cart.add({ name: '🍋', price: 60 })
+cart.add({ name: '🍋', price: 60 })
+cart.add({ name: '🍋', price: 60 })
+cart.add({ name: '🍓', price: 110 })
+cart.add({ name: '🍓', price: 110 })
+cart.add({ name: '🍓', price: 110 })
 cart.add({ name: '🍓', price: 110 })
 cart.add({ name: '🍎', price: 50 })
+cart.add({ name: '🍎', price: 50 })
+cart.add({ name: '🍎', price: 50 })
+cart.add({ name: '🍎', price: 50 })
+
 
 console.table(cart.getItems());
+
+console.log('Total: ', cart.countTotalPrice());
 
 cart.remove('🍋')
 console.table(cart.getItems());
+console.log('Total: ', cart.countTotalPrice());
 
 cart.clear();
 console.log(cart.getItems());
+console.log('Total: ', cart.countTotalPrice());
+
